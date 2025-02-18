b769044ab1e3c78a8bb617d2f7a1043faac72fb6
Naim000007 <mdnaim01303202218@gmail.com>
2025-02-18 11:01:46 +0600

    react tostify add and add to cart added

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,5 +1,6 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/frontend_assets/assets";
+import { toast } from "react-toastify";
 
 export const ShopContext = createContext()
 
@@ -8,8 +9,84 @@ const ShopContextProvider = (props) => {
     const delivery_fee = 60;
     const [search, setSearch] = useState([])
     const [showSearch, setShowSearch] = useState(false)
+    const [cartItems, setCartItems] = useState({})
+
+    // const addToCart = async (itemId, size) => {
+    //     if (!size) {
+    //         toast.error('Select Product Size')
+    //         return
+    //     }
+    //     let cartData = structuredClone(cartItems)
+
+    //     if (cartData[itemId]) {
+    //         if (cartData[itemId][size]) {
+    //             cartData[itemId][size] += 1
+    //         }
+    //         else {
+    //             cartData[itemId][size] = 1
+    //         }
+    //     }
+    //     else {
+    //         cartData[itemId] = {}
+    //         cartData[itemId][size] = 1
+    //     }
+    //     setCartItems(cartData)
+    // }
+    const addToCart = (itemId, size) => {
+        if (!size) {
+            toast.error('Select Product Size');
+            return;
+        }
+
+        setCartItems((prevCart) => {
+            let cartData = structuredClone(prevCart);
+
+            if (cartData[itemId]) {
+                if (cartData[itemId][size]) {
+                    cartData[itemId][size] += 1;
+                } else {
+                    cartData[itemId][size] = 1;
+                }
+            } else {
+                cartData[itemId] = {};
+                cartData[itemId][size] = 1;
+            }
+            console.log(cartData);
+
+            return cartData;
+        });
+    };
+
+
+    // const getCartCount = () => {
+    //     let totalCount = 0;
+    //     for (const items in cartItems) {
+    //         for (const item in cartItems[items]) {
+    //             try {
+    //                 if (cartItems[i][item] > 0) {
+    //                     totalCount += cartItems[items][item]
+    //                 }
+    //             } catch (error) {
+
+    //             }
+    //         }
+    //     }
+    //     return totalCount;
+    // }
+    const getCartCount = () => {
+        let totalCount = 0;
+        for (const itemId in cartItems) {
+            for (const size in cartItems[itemId]) {
+                if (cartItems[itemId][size] > 0) {
+                    totalCount += cartItems[itemId][size];
+                }
+            }
+        }
+        return totalCount;
+    };
+
     const value = {
-        products, currency, delivery_fee, search, setSearch, showSearch, setShowSearch
+        products, currency, delivery_fee, search, setSearch, showSearch, setShowSearch, cartItems, addToCart, getCartCount
     }
     return (
         <ShopContext.Provider value={value}>
